55b09d5205e7acf63302a4fc6328b5e94ef0852b
AttalliAyoub <attalliayoub50@gmail.com>
2020-03-13 17:28:43 +0100

    secound connit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ToolbarComponent } from './toolbar/toolbar.component';
+// material modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MatSelectModule } from '@angular/material/select';
+// material modules end
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PageNotFoundComponentComponent
+    PageNotFoundComponentComponent,
+    ToolbarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // material modules
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule
+    // material modules end
   ],
   providers: [],
   bootstrap: [AppComponent]
